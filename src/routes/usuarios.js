5c753ce59eb997896ab1b1171008a5212e044170
MATHEUS FERNANDO FREIRE FERREIRA <matheusfernandofreire@hotmail.com>
2021-05-08 12:48:32 -0300

    Criação de Rota de update

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -4,6 +4,7 @@ const UsuarioController = require("../controllers/UsuarioController");
 const router = express.Router(); // Importação do router do express.
 
 router.post("/", UsuarioController.create);
+router.put("/", UsuarioController.update);
 router.get("/", UsuarioController.index);
 router.get("/user-ver/:username", UsuarioController.findByUsername);
 router.get("/:id", UsuarioController.show);
